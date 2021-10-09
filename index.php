baa184ad71ee3a3f9162fa0206b197d93a6fb76b
Rakesh Saha <rakeshsaha87550@gmail.com>
2021-10-09 21:38:04 +0530

    add all files

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Facbook-login or signup</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> <!--bootstrap css-->
+	<link rel="shortcut icon" href="images/capture3.png"> <!-- favicon-->
+	<link rel="stylesheet" type="text/css" href="css/style.css"> <!--custom css-->
+</head>
+
+<body>
+	<nav class="fbheader" style="padding: 0px 184px opx 150px;">
+		<!--maindiv-->
+		<div class="container">
+			<!--facebooklogo-->
+			<div class="row">
+				<div class="col-7" style="margin-top: 29px; margin-left: -10px;">
+					<img src="images/Capture1.jpg" alt="Error" height="50px" width="190px">
+				</div>
+				<div class="col-5" style="margin-top: 2px;">
+					<!--login-->
+					<div class="row">
+						<div class="col-5">
+							<lebel>Email or Phone</lebel>
+						</div>
+						<div class="col-5">
+							<lebel>Password</lebel>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-5">
+							<input type="text" name="" class="form">
+						</div>
+						<div class="col-5">
+							<input type="text" name="" class="form">
+						</div>
+						<div class="col-2">
+							<button type="submit" name="submit" class="btn btn-primary">LogIn</button>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-5">
+
+						</div>
+						<div class="col-5">
+							<a href="#" style="color:#9cb4d8;">Forgotten account?</a>
+						</div>
+					</div>
+				</div>
+				<!--end of login-->
+			</div>
+		</div>
+	</nav>
+	<!--end of header-->
+	<section style="background-image:linear-gradient(#FDFDFF,#D5D8E9); height: 600px; padding: 30px 150px;">
+		<div class="container">
+			<!-- start body-->
+			<div class="row">
+				<div class="col-6" style="margin-left: 20px;">
+					<h5 style="color:#0e385f;">Facebook helps you connect and share with the people in your life.</h5>
+					<br>
+					<img src="images/capture2.jpg" alt="Error" width="535px" height="190px">
+				</div>
+				<div class="col-5" style="margin-left: 65px;">
+					<!--start signup-->
+					<h1>Create an account</h1>
+					<h5>It's free and always will be.</h5><br>
+					<div class="row">
+						<div class="col-6">
+							<input type="text" name="" placeholder="First name" class="signup">
+						</div>
+						<div class="col-6">
+							<input type="text" name="" placeholder="surname" class="signup">
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class="col-4">
+							<h5>Birthday</h5>
+						</div>
+						<div class="col-8">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-6">
+							<div class="row">
+								<div class="col-4">
+									<select>
+										<option>1 </option>
+										<option>2 </option>
+										<option>3 </option>
+										<option>4 </option>
+										<option>5 </option>
+									</select>
+								</div>
+								<div class="col-4">
+									<select>
+										<option>Jan </option>
+										<option>Feb </option>
+										<option>Mar</option>
+										<option>Apr </option>
+										<option>Jun</option>
+									</select>
+								</div>
+								<div class="col-4">
+									<select>
+										<option>2000</option>
+										<option>2001</option>
+										<option>2002</option>
+										<option>2003</option>
+										<option>2004</option>
+										<option>2005</option>
+									</select>
+								</div>
+							</div>
+						</div>
+						<div class="col-5">
+							<a href="#" style="color:#5E6F8D;font-size:12px;">Why do I need to provide my date of
+								birth</a>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-9">
+							<form class="radio">
+								<input type="radio" name="fradio">&nbsp;Female &emsp;
+								<input type="radio" name="fradio">&nbsp;Male
+							</form>
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class="col-10">
+							<p style="color:#7485A3;font-size: 12px;">By clicking Sign Up, you agree to our Terms, Data
+								Policy and Cookie Policy. You may receive SMS notifications from us and can opt out at
+								any time.</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-8">
+							<button class="btn">Sign up</button>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-8">
+							<br>
+							<p style="color: #000;font-size: 13px;">Create a Page for a celebrity, band or business.</p>
+						</div>
+					</div>
+				</div>
+				<!--end of signup-->
+			</div>
+		</div>
+		<!--end of body-->
+	</section>
+	<footer style="padding: 10px 0px;">
+		<!--start footer-->
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<ul class="list">
+						<li class="float-left">English &nbsp&nbsp</li>
+						<li class="float-left">বাংলা &nbsp&nbsp</li>
+						<li class="float-left">हिन्दी &nbsp&nbsp</li>
+						<li class="float-left">اردو &nbsp&nbsp</li>
+						<li class="float-left">नेपाली &nbsp&nbsp</li>
+						<li class="float-left">ଓଡ଼ିଆ &nbsp&nbsp</li>
+						<li class="float-left">Español &nbsp&nbsp</li>
+						<li class="float-left">Português (Brasil) &nbsp&nbsp</li>
+						<li class="float-left">Deutsch &nbsp&nbsp</li>
+						<li class="float-left">Italiano &nbsp&nbsp</li>
+					</ul>
+				</div>
+			</div>
+			<hr width="1050px">
+			<div class="row">
+				<div class="col-12">
+					<ul class="list">
+						<li class="float-left">Sign &nbsp&nbsp</li>
+						<li class="float-left">Log In &nbsp&nbsp</li>
+						<li class="float-left">Messenger &nbsp&nbsp</li>
+						<li class="float-left">Facebook Lite &nbsp&nbsp</li>
+						<li class="float-left">Find Friends &nbsp&nbsp</li>
+						<li class="float-left">People &nbsp&nbsp</li>
+						<li class="float-left">ProfilesPages &nbsp&nbsp</li>
+						<li class="float-left">Page categories &nbsp&nbsp</li>
+						<li class="float-left">Places &nbsp&nbsp</li>
+						<li class="float-left">Games &nbsp&nbsp</li>
+						<li class="float-left">Locations &nbsp&nbsp</li>
+						<li class="float-left">Marketplace &nbsp&nbsp</li>
+						<li class="float-left">Groups &nbsp&nbsp</li>
+						<li class="float-left">Instagram &nbsp&nbsp</li>
+						<li class="float-left">Local &nbsp&nbsp</li>
+						<li class="float-left">Fundraisers &nbsp&nbsp</li>
+						<li class="float-left">About &nbsp&nbsp</li>
+						<li class="float-left">Settings &nbsp&nbsp</li>
+					</ul>
+				</div>
+			</div>
+	</footer>
+    <!--end of footer-->
+    <!-- scripts -->
+    <script src="css/bootstrap.min.js"></script>
+</body>
+
+</html>
